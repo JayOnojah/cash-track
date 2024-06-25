8b6d4e010d566086228ce64c287f7efa4bacf57f
Jeremiah <jayonojah@gmail.com>
2024-06-25 15:03:58 +0100

    Accounts GET API Completion

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 
