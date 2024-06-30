c394313a206c1c6fa34ce961ce3a729c554932cd
Jeremiah <jayonojah@gmail.com>
2024-06-30 19:18:18 +0100

    Account Form Set up

---
+++ features/accounts/components/account-form.tsx
@@ -0,0 +1,56 @@
+import { z } from "zod";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { insertAccountSchema } from "@/db/schema";
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+type Props = {
+  id?: string;
+  defaultValues?: FormValues;
+  onSubmit: (values: FormValues) => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+};
+
+export const AccountForm = ({
+  id,
+  defaultValues,
+  onSubmit,
+  onDelete,
+  disabled,
+}: Props) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: defaultValues,
+  });
+
+  const handleSubmit = (values: FormValues) => {
+    console.log({ values });
+  };
+
+  const handleDelete = () => {
+    onDelete?.();
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)}></form>
+    </Form>
+  );
+};
