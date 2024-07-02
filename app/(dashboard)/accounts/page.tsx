39d1cf61c11969b62764ddaa3a9bc7f27edb2b05
Jeremiah <jayonojah@gmail.com>
2024-07-02 01:00:59 +0100

    Accounts Bulk Delete Completion

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,47 +1,42 @@
 "use client";
 
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-
-import { Payment, columns } from "./columns";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useBultDeleteAccounts } from "@/features/accounts/api/use-bulk-delete";
 
 import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/data-table";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-const data: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 200,
-    status: "success",
-    email: "pt@example.com",
-  },
-
-  {
-    id: "728ed52f",
-    amount: 70,
-    status: "pending",
-    email: "mj@example.com",
-  },
-
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "processing",
-    email: "mt@example.com",
-  },
-
-  {
-    id: "728ed52f",
-    amount: 80,
-    status: "pending",
-    email: "jj@example.com",
-  },
-];
+import { columns } from "./columns";
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
+  const deleteAccounts = useBultDeleteAccounts();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+
+  const isDisabled = accountsQuery.isLoading || deleteAccounts.isPending;
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -57,9 +52,12 @@ const AccountsPage = () => {
           <DataTable
             filterKey="email"
             columns={columns}
-            data={data}
-            onDelete={() => {}}
-            disabled={false}
+            data={accounts}
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id);
+              deleteAccounts.mutate({ ids });
+            }}
+            disabled={isDisabled}
           />
         </CardContent>
       </Card>
