623069d4d104f5491fffddc2775f873b626cc86e
Jeremiah <jayonojah@gmail.com>
2024-07-02 13:03:28 +0100

    Delete Account Completion

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -1,26 +1,43 @@
 "use client";
 
-import { Edit, MoreHorizontal } from "lucide-react";
+import { Trash, Edit, MoreHorizontal } from "lucide-react";
 
 import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
 
-import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuItem,
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { useConfirm } from "@/hooks/use-confirm";
 
 type Props = {
   id: string;
 };
 
 export const Actions = ({ id }: Props) => {
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You are about to delete this account. This action cannot be undone."
+  );
+
+  const deleteMutation = useDeleteAccount(id);
   const { onOpen } = useOpenAccount();
+
+  const handleDelete = async () => {
+    const ok = await confirm();
+
+    if (ok) {
+      deleteMutation.mutate();
+    }
+  };
 
   return (
     <>
+      <ConfirmDialog />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="size-8 p-0">
@@ -28,8 +45,15 @@ export const Actions = ({ id }: Props) => {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-          <DropdownMenuItem disabled={false} onClick={() => onOpen(id)}>
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={() => onOpen(id)}>
             <Edit className="size-4 mr-2" /> Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={handleDelete}>
+            <Trash className="size-4 mr-2" /> Delete
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
